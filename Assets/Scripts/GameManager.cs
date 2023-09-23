3e1f42b09e29590481a83a749f2ca4bd5d4c32de
Fawad <fawadshah501@gmail.com>
2023-09-23 23:52:51 +0500

    Visual Studio Demo done

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private void Start()
+    {
+        Debug.Log("Hello! Visual studio linked successfully");
+    }
     public void Play()
     {
         SceneManager.LoadScene("Level1");
